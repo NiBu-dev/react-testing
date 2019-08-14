6f6db9335aeb2cd87b3df966206236a5d7f54093
Nick <sahas023@gmail.com>
2019-08-14 22:50:59 +0200

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Header from './components/header';
+import './App.scss';
 function App() {
   return (
     <div className="App">
-      Test
+      <Header/>
     </div>
   );
 }
